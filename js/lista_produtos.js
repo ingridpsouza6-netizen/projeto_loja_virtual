88f79e45270146584ba534617fc717a0ac2f1666
Ingrid Souza <ingrid.p.souza6@aluno.senai.br>
2026-07-09 16:46:00 -0300

    adicionado novos textos

---
+++ js/lista_produtos.js
@@ -0,0 +1,182 @@
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_produto: "Vale das Flores",
+        valor_unitario: 699.90,
+        unidade: "un",
+        caminho_imagem: "imagens/vale-das-flores.jpg",
+        id_secao: 1,
+        secao: "Paisagens"
+    },
+    {
+        id_produto: 2,
+        descricao_produto: "Montanhas Nevadas",
+        valor_unitario: 1099.90,
+        unidade: "un",
+        caminho_imagem: "imagens/montanhas-nevadas.jpg",
+        id_secao: 2,
+        secao: "Montanhas"
+    },
+    {
+        id_produto: 3,
+        descricao_produto: "Lago Cristalino",
+        valor_unitario: 849.90,
+        unidade: "un",
+        caminho_imagem: "imagens/lago-cristalino.jpg",
+        id_secao: 3,
+        secao: "Lagos"
+    },
+    {
+        id_produto: 4,
+        descricao_produto: "Floresta Encantada",
+        valor_unitario: 959.90,
+        unidade: "un",
+        caminho_imagem: "imagens/floresta-encantada.jpg",
+        id_secao: 4,
+        secao: "Florestas"
+    },
+    {
+        id_produto: 5,
+        descricao_produto: "Pôr do Sol Escarlate",
+        valor_unitario: 599.90,
+        unidade: "un",
+        caminho_imagem: "imagens/por-do-sol-escarlate.jpg",
+        id_secao: 5,
+        secao: "Pôr do Sol"
+    },
+    {
+        id_produto: 6,
+        descricao_produto: "Campo de Lavandas",
+        valor_unitario: 789.90,
+        unidade: "un",
+        caminho_imagem: "imagens/campo-de-lavandas.jpg",
+        id_secao: 6,
+        secao: "Campos"
+    },
+    {
+        id_produto: 7,
+        descricao_produto: "Aurora Boreal",
+        valor_unitario: 1399.90,
+        unidade: "un",
+        caminho_imagem: "imagens/aurora-boreal.jpg",
+        id_secao: 7,
+        secao: "Céu"
+    },
+    {
+        id_produto: 8,
+        descricao_produto: "Rio das Montanhas",
+        valor_unitario: 949.90,
+        unidade: "un",
+        caminho_imagem: "imagens/rio-das-montanhas.jpg",
+        id_secao: 8,
+        secao: "Rios"
+    },
+    {
+        id_produto: 9,
+        descricao_produto: "Deserto Dourado",
+        valor_unitario: 899.90,
+        unidade: "un",
+        caminho_imagem: "imagens/deserto-dourado.jpg",
+        id_secao: 9,
+        secao: "Desertos"
+    },
+    {
+        id_produto: 10,
+        descricao_produto: "Jardim de Tulipas – Holanda",
+        valor_unitario: 999.90,
+        unidade: "un",
+        caminho_imagem: "imagens/jardim-de-tulipas-holanda.jpg",
+        id_secao: 10,
+        secao: "Jardins"
+    },
+    {
+        id_produto: 11,
+        descricao_produto: "Ilha da Serenidade",
+        valor_unitario: 899.90,
+        unidade: "un",
+        caminho_imagem: "imagens/ilha-da-serenidade.jpg",
+        id_secao: 11,
+        secao: "Ilhas"
+    },
+    {
+        id_produto: 12,
+        descricao_produto: "Mar Dourado",
+        valor_unitario: 729.90,
+        unidade: "un",
+        caminho_imagem: "imagens/mar-dourado.jpg",
+        id_secao: 12,
+        secao: "Mares"
+    },
+    {
+        id_produto: 13,
+        descricao_produto: "Praia de Águas Cristalinas",
+        valor_unitario: 819.90,
+        unidade: "un",
+        caminho_imagem: "imagens/praia-de-aguas-cristalinas.jpg",
+        id_secao: 13,
+        secao: "Praias"
+    },
+    {
+        id_produto: 14,
+        descricao_produto: "Outono à Beira do Lago",
+        valor_unitario: 879.90,
+        unidade: "un",
+        caminho_imagem: "imagens/outono-a-beira-do-lago.jpg",
+        id_secao: 3,
+        secao: "Lagos"
+    },
+    {
+        id_produto: 15,
+        descricao_produto: "Montanhas Azuis",
+        valor_unitario: 1089.90,
+        unidade: "un",
+        caminho_imagem: "imagens/montanhas-azuis.jpg",
+        id_secao: 2,
+        secao: "Montanhas"
+    },
+    {
+        id_produto: 16,
+        descricao_produto: "Lago Turquesa",
+        valor_unitario: 1249.90,
+        unidade: "un",
+        caminho_imagem: "imagens/lago-turquesa.jpg",
+        id_secao: 3,
+        secao: "Lagos"
+    },
+    {
+        id_produto: 17,
+        descricao_produto: "Cachoeira Encantada",
+        valor_unitario: 969.90,
+        unidade: "un",
+        caminho_imagem: "imagens/cachoeira-encantada.jpg",
+        id_secao: 14,
+        secao: "Cachoeiras"
+    },
+    {
+        id_produto: 18,
+        descricao_produto: "Geleira Azul",
+        valor_unitario: 1279.90,
+        unidade: "un",
+        caminho_imagem: "imagens/geleira-azul.jpg",
+        id_secao: 15,
+        secao: "Geleiras"
+    },
+    {
+        id_produto: 19,
+        descricao_produto: "Praia Esmeralda",
+        valor_unitario: 929.90,
+        unidade: "un",
+        caminho_imagem: "imagens/praia-esmeralda.jpg",
+        id_secao: 13,
+        secao: "Praias"
+    },
+    {
+        id_produto: 20,
+        descricao_produto: "Horizonte Dourado",
+        valor_unitario: 639.90,
+        unidade: "un",
+        caminho_imagem: "imagens/horizonte-dourado.jpg",
+        id_secao: 5,
+        secao: "Pôr do Sol"
+    }
+];
