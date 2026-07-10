1ca68e022387ccfab0f961fc1f78e1c7a9dd7b8f
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-10 16:45:38 -0300

    adicionado novos textos

--- js/lista_produtos.js
+++ js/lista_produtos.js
@@ -1,12 +1,12 @@
 const produtos = [
     {
         id_produto: 1,
-        descricao_produto: "Vale das Flores",
+        descricao_produto: "Caminho das Flores",
         valor_unitario: 699.90,
         unidade: "un",
-        caminho_imagem: "imagens/vale-das-flores.jpg",
+        caminho_imagem: "imagens/Caminho das-Flores.jpg",
         id_secao: 1,
-        secao: "Paisagens"
+        secao: "Paisagens Floridas"
     },
     {
         id_produto: 2,
@@ -14,8 +14,8 @@ const produtos = [
         valor_unitario: 1099.90,
         unidade: "un",
         caminho_imagem: "imagens/montanhas-nevadas.jpg",
-        id_secao: 2,
-        secao: "Montanhas"
+        id_secao: 3,
+        secao: "Montanhas e Geleiras"
     },
     {
         id_produto: 3,
@@ -23,8 +23,8 @@ const produtos = [
         valor_unitario: 849.90,
         unidade: "un",
         caminho_imagem: "imagens/lago-cristalino.jpg",
-        id_secao: 3,
-        secao: "Lagos"
+        id_secao: 4,
+        secao: "Lagos e Cachoeiras"
     },
     {
         id_produto: 4,
@@ -32,8 +32,8 @@ const produtos = [
         valor_unitario: 959.90,
         unidade: "un",
         caminho_imagem: "imagens/floresta-encantada.jpg",
-        id_secao: 4,
-        secao: "Florestas"
+        id_secao: 1,
+        secao: "Paisagens Floridas"
     },
     {
         id_produto: 5,
@@ -42,7 +42,7 @@ const produtos = [
         unidade: "un",
         caminho_imagem: "imagens/por-do-sol-escarlate.jpg",
         id_secao: 5,
-        secao: "Pôr do Sol"
+        secao: "Céus e Paisagens Naturais"
     },
     {
         id_produto: 6,
@@ -50,8 +50,8 @@ const produtos = [
         valor_unitario: 789.90,
         unidade: "un",
         caminho_imagem: "imagens/campo-de-lavandas.jpg",
-        id_secao: 6,
-        secao: "Campos"
+        id_secao: 1,
+        secao: "Paisagens Floridas"
     },
     {
         id_produto: 7,
@@ -59,8 +59,8 @@ const produtos = [
         valor_unitario: 1399.90,
         unidade: "un",
         caminho_imagem: "imagens/aurora-boreal.jpg",
-        id_secao: 7,
-        secao: "Céu"
+        id_secao: 5,
+        secao: "Céus e Paisagens Naturais"
     },
     {
         id_produto: 8,
@@ -68,8 +68,8 @@ const produtos = [
         valor_unitario: 949.90,
         unidade: "un",
         caminho_imagem: "imagens/rio-das-montanhas.jpg",
-        id_secao: 8,
-        secao: "Rios"
+        id_secao: 3,
+        secao: "Montanhas e Geleiras"
     },
     {
         id_produto: 9,
@@ -77,8 +77,8 @@ const produtos = [
         valor_unitario: 899.90,
         unidade: "un",
         caminho_imagem: "imagens/deserto-dourado.jpg",
-        id_secao: 9,
-        secao: "Desertos"
+        id_secao: 5,
+        secao: "Céus e Paisagens Naturais"
     },
     {
         id_produto: 10,
@@ -86,8 +86,8 @@ const produtos = [
         valor_unitario: 999.90,
         unidade: "un",
         caminho_imagem: "imagens/jardim-de-tulipas-holanda.jpg",
-        id_secao: 10,
-        secao: "Jardins"
+        id_secao: 1,
+        secao: "Paisagens Floridas"
     },
     {
         id_produto: 11,
@@ -95,8 +95,8 @@ const produtos = [
         valor_unitario: 899.90,
         unidade: "un",
         caminho_imagem: "imagens/ilha-da-serenidade.jpg",
-        id_secao: 11,
-        secao: "Ilhas"
+        id_secao: 2,
+        secao: "Praias e Ilhas"
     },
     {
         id_produto: 12,
@@ -104,8 +104,8 @@ const produtos = [
         valor_unitario: 729.90,
         unidade: "un",
         caminho_imagem: "imagens/mar-dourado.jpg",
-        id_secao: 12,
-        secao: "Mares"
+        id_secao: 2,
+        secao: "Praias e Ilhas"
     },
     {
         id_produto: 13,
@@ -113,8 +113,8 @@ const produtos = [
         valor_unitario: 819.90,
         unidade: "un",
         caminho_imagem: "imagens/praia-de-aguas-cristalinas.jpg",
-        id_secao: 13,
-        secao: "Praias"
+        id_secao: 2,
+        secao: "Praias e Ilhas"
     },
     {
         id_produto: 14,
@@ -122,8 +122,8 @@ const produtos = [
         valor_unitario: 879.90,
         unidade: "un",
         caminho_imagem: "imagens/outono-a-beira-do-lago.jpg",
-        id_secao: 3,
-        secao: "Lagos"
+        id_secao: 4,
+        secao: "Lagos e Cachoeiras"
     },
     {
         id_produto: 15,
@@ -131,8 +131,8 @@ const produtos = [
         valor_unitario: 1089.90,
         unidade: "un",
         caminho_imagem: "imagens/montanhas-azuis.jpg",
-        id_secao: 2,
-        secao: "Montanhas"
+        id_secao: 3,
+        secao: "Montanhas e Geleiras"
     },
     {
         id_produto: 16,
@@ -140,8 +140,8 @@ const produtos = [
         valor_unitario: 1249.90,
         unidade: "un",
         caminho_imagem: "imagens/lago-turquesa.jpg",
-        id_secao: 3,
-        secao: "Lagos"
+        id_secao: 4,
+        secao: "Lagos e Cachoeiras"
     },
     {
         id_produto: 17,
@@ -149,8 +149,8 @@ const produtos = [
         valor_unitario: 969.90,
         unidade: "un",
         caminho_imagem: "imagens/cachoeira-encantada.jpg",
-        id_secao: 14,
-        secao: "Cachoeiras"
+        id_secao: 4,
+        secao: "Lagos e Cachoeiras"
     },
     {
         id_produto: 18,
@@ -158,8 +158,8 @@ const produtos = [
         valor_unitario: 1279.90,
         unidade: "un",
         caminho_imagem: "imagens/geleira-azul.jpg",
-        id_secao: 15,
-        secao: "Geleiras"
+        id_secao: 3,
+        secao: "Montanhas e Geleiras"
     },
     {
         id_produto: 19,
@@ -167,8 +167,8 @@ const produtos = [
         valor_unitario: 929.90,
         unidade: "un",
         caminho_imagem: "imagens/praia-esmeralda.jpg",
-        id_secao: 13,
-        secao: "Praias"
+        id_secao: 2,
+        secao: "Praias e Ilhas"
     },
     {
         id_produto: 20,
@@ -177,6 +177,6 @@ const produtos = [
         unidade: "un",
         caminho_imagem: "imagens/horizonte-dourado.jpg",
         id_secao: 5,
-        secao: "Pôr do Sol"
+        secao: "Céus e Paisagens Naturais"
     }
-];
+]
